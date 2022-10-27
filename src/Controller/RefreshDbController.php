796219d0341157a9ddc50be61f4a1249406c58b5
Johann six <six.johann@gmail.com>
2022-10-27 09:54:24 +0200

    🚧 reload the db from external api

--- src/Controller/RefreshDbController.php
+++ src/Controller/RefreshDbController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Rubbish;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\CurlHttpClient;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
@@ -25,6 +26,7 @@ class RefreshDbController extends AbstractController
         $currentDate = new \DateTimeImmutable("now");
         if($pwd === $_ENV['REFRESH_DB_PASSWORD']){
             $list = $this->fetchFromApi();
+            // $list = $this->testApi();
             return $this->json($list);
             // foreach($list as $brewery){
             //     $toAdd = $this->newBrewery($brewery, $currentDate);
@@ -43,11 +45,32 @@ class RefreshDbController extends AbstractController
     {
         $response = $this->client->request(
             'GET',
-            'https://data.metropole-rouen-normandie.fr/api/v2/catalog/datasets/donmetdec_pav/records?limit=10&offset=0&timezone=UTC'
+            'https://data.metropole-rouen-normandie.fr/api/v2/catalog/datasets/donmetdec_pav/records?limit=10&offset=0&timezone=UTC',
+            [
+                'headers' => [
+                    'accept' => 'application/json',
+                    'charset' => 'utf-8',
+                ],
+            ]
         );
-
+        $statusCode = $response->getStatusCode();
+        return $statusCode;
+        $httpLogs = $response->getInfo();
+        return $httpLogs;
         $content = $response->toArray();
         return $content;
 
+    }
+    public function testApi()
+    {
+        $client = new CurlHttpClient();
+        $url = "https://data.metropole-rouen-normandie.fr/api/v2/catalog/datasets/donmetdec_pav/records?limit=10&offset=0&timezone=UTC";
+        $request = $client->request('GET', $url, [
+            'headers' => [
+                'accept' => 'application/json',
+                'charset' => 'utf-8',
+            ],
+        ]);
+        $request->getContent();
     }
 }
